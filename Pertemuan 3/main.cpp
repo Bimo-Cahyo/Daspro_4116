97a503fe99e4a73e89c06061000dfd49dff7bde0
BimoCahyo <111202214610@mhs.dinus.com>
2022-09-20 23:37:02 +0700

    first commit

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+   /*
+   Nama: Bimo
+   Nim: A11.2022.14610
+   */
+
+     int u; //ddeklarasi
+     u= 20;
+
+     float t = 165;//inisiallisasi
+     char ub ='X';
+     string ubs ="XL";
+
+
+     cout <<"Umur Saya:"<<u;
+     cout << endl;
+     cout << "Tinggi Saya:" << t;
+     cout << "\nUkuran Baju:" << ubs;
+
+     int a;
+     cout << "\nMasukan angka: ";
+     cin>>a;
+     cout << "angka anda + 17: " << a + u ;
+    return 0;
+}
---
+++ tugas 1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()s
+{
+    cout << "Hello world!" << endl;
+    cout << "Nama : Bimo Cahyo Widyanto" << endl;
+    cout << "TTL : Tegal,19 Juli 2002" << endl;
+    cout << "Alamat : JL.Todak, No. 03" << endl;
+    cout << "Gol. Darah: O" << endl;
+    cout << "Jenis Kelamin : Laki- Laki" << endl;
+    cout << "Agama : Islam" << endl;
+    cout << "NIM : A11.2022.14610" << endl;
+    cout << "Asal Sekolah : SMA Al-Irsyad Kota Tegal" << endl;
+    return 0;
+}
