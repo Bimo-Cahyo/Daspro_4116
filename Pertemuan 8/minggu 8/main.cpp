ee3be514cd42c5bc49ae259ff18dec109e9581e9
Bimo-Cahyo <113614066+Bimo-Cahyo@users.noreply.github.com>
2022-10-05 12:19:13 +0700

    Add files via upload

---
+++ Pertemuan 8/minggu 8/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int hari;
+    int bulan;
+    cout << "masukkan hari :/n";
+    cin >> hari;
+    cout << "masukkan bulan :/n";
+    cin >> bulan;
+
+    bool bulanGanjil = bulan % 2 =1;
+    int hariMaximal;
+
+    if (hari <=30) {
+        cout << "Tanggal Valid" << endl;
+    } else {
+        cout << "Tanggal Tidak Valid" << endl;
+    }
+
+
+    return 0;
+}
---
+++ Pertemuan 8/tugas 8/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*int day;
+    cin >> day;
+
+    switch (day) {
+        case 1:
+            cout << "Satu";
+            break;
+        case 2:
+            cout << "Dua";
+            break;
+        default:
+        cout << "Underfired";
+        break;
+}       */
+    int x = 0;
+    for(x = 0; x < 9001; x++){
+        cout << x << " Saya tidak terlambat lagi"b  ;
+        x++;
+
+    }
+    return 0;
+}
